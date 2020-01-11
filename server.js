ed6dbc3b0964ad01de76baaa460fcd98532c5292
Kingsleigh Crown <20748544+edenhack@users.noreply.github.com>
2020-01-10 21:32:14 -0600

    added all files and types

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// parse application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+// parse application/json
+app.use(expresss.json());
+
+// Set Handlebars.
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+const routes = require("./controllers/burger_controller.js");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+    console.log("Server listening on: http://localhost:" + PORT);
+});
